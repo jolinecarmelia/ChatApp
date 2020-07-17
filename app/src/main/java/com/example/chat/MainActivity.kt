580e5ba4a16cf16146fd8dacbfad7ac329d7ad28
jolinecarmelia <jolinetee@gmail.com>
2020-07-18 02:23:21 +0800

    from Joline

--- app/src/main/java/com/example/chat/MainActivity.kt
+++ app/src/main/java/com/example/chat/MainActivity.kt
@@ -2,16 +2,14 @@ package com.example.chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val email = email_editText_register.text.toString()
-        val password = password_edittext_register.text.toString()
-
-        Log.d(tag: "MainActivity", msg: "Email is: " + email)
-        Log.d(tag: "MainActivity", msg: "Password is: $password")
+        Log.d("MainActivity", "Email is:" + email)
+        Log.d("MainActivity", "Password is: $password")
     }
 }
