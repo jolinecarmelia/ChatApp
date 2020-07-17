2268a55e6d3c2401f1cd8e6ca73ba2283a6fb201
jolinecarmelia <jolinetee@gmail.com>
2020-07-18 01:08:47 +0800

    from Joline

--- app/src/main/java/com/example/chat/MainActivity.kt
+++ app/src/main/java/com/example/chat/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val email = email_edittext_register.text.toString()
+        val email = email_editText_register.text.toString()
         val password = password_edittext_register.text.toString()
 
         Log.d(tag: "MainActivity", msg: "Email is: " + email)
